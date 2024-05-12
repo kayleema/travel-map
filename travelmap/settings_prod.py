2a7b299b554b194c55fcfbd75e98cfd8feb81724
Kaylee Mann <kayleemann7@gmail.com>
2024-05-13 00:12:15 +0900

    add production config

---
+++ travelmap/settings_prod.py
@@ -0,0 +1,12 @@
+from travelmap.settings import *
+import os
+
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql_psycopg2',
+		'NAME': os.getenv('DATABASE_NAME'),
+		'USER': os.getenv('DATABASE_USER'),
+		'PASSWORD': os.getenv('DATABASE_PASS'),
+		'HOST': os.getenv('DATABASE_HOST')
+	}
+}
