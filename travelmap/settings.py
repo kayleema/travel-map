76d19de10d210e4c4b9b318067256950de5ec50b
Kaylee Mann <kayleemann7@gmail.com>
2024-05-13 13:12:13 +0900

    fix icon

--- travelmap/settings.py
+++ travelmap/settings.py
@@ -146,6 +146,10 @@ LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
